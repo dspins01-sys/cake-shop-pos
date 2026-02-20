0a83e9e4c35a085cca298ed668e0d8df0fb1eb6e
dspins01-sys <dspins01-sys@gmail.com>
2026-02-20 14:36:00 +0700

    Initial commit: update

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -38,55 +38,111 @@
     </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="{{ route('home') }}">SweetCake</a>
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('home') }}">Home</a>
-                    </li>
-                    
-                    <!-- CART ICON - UNTUK SEMUA (dikasih comment biar gampang dicari) -->
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('cart.index') }}">
-                            <i class="fas fa-shopping-cart"></i>
-                            @php
-                                $cartCount = 0;
-                                if (class_exists('App\Helpers\CartHelper')) {
-                                    $cartCount = App\Helpers\CartHelper::totalItems();
-                                }
-                            @endphp
-                            @if($cartCount > 0)
-                                <span class="badge bg-danger">{{ $cartCount }}</span>
-                            @endif
+    <!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('home') }}">
+            <i class="fas fa-cake-candles"></i> SweetCake
+        </a>
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <!-- Left Menu -->
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('home') }}">Home</a>
+                </li>
+                <!-- Categories -->
+                @php
+                    $categories = App\Models\Category::all();
+                @endphp
+                @foreach($categories as $cat)
+                    <li class="nav-item d-none d-lg-block">
+                        <a class="nav-link" href="{{ route('products.category', $cat->slug) }}">
+                            {{ $cat->name }}
                         </a>
                     </li>
+                @endforeach
+            </ul>
 
-                    @auth
-                        <li class="nav-item">
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <button type="submit" class="btn btn-link nav-link">Logout ({{ Auth::user()->name }})</button>
-                            </form>
-                        </li>
-                    @else
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">Register</a>
-                        </li>
-                        <li class="nav-item">
-    <a class="nav-link" href="{{ route('order.track.form') }}">
-        <i class="fas fa-search"></i> Track Order
-    </a>
-</li>
-                    @endauth
-                </ul>
-            </div>
+            <!-- Right Menu -->
+            <ul class="navbar-nav align-items-center">
+                <!-- TRACK ORDER LINK - TAMBAHKAN INI -->
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('order.track.form') }}">
+                        <i class="fas fa-search"></i>
+                        <span class="d-none d-lg-inline ms-1">Track Order</span>
+                    </a>
+                </li>
+
+                <!-- Cart Icon -->
+                <li class="nav-item position-relative me-2">
+                    <a class="nav-link" href="{{ route('cart.index') }}">
+                        <i class="fas fa-shopping-cart"></i>
+                        @php
+                            $cartCount = 0;
+                            if (class_exists('App\Helpers\CartHelper')) {
+                                $cartCount = App\Helpers\CartHelper::totalItems();
+                            }
+                        @endphp
+                        @if($cartCount > 0)
+                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                {{ $cartCount }}
+                            </span>
+                        @endif
+                    </a>
+                </li>
+
+                @auth
+                    <!-- User Dropdown -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown">
+                            <i class="fas fa-user-circle"></i>
+                            <span class="d-none d-lg-inline ms-1">{{ Auth::user()->name }}</span>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            @if(Auth::user()->role == 'admin')
+                                <li><a class="dropdown-item" href="{{ route('admin.products.index') }}">
+                                    <i class="fas fa-cog me-2"></i>Admin Panel
+                                </a></li>
+                                <li><hr class="dropdown-divider"></li>
+                            @endif
+                            <li><a class="dropdown-item" href="{{ route('profile.edit') }}">
+                                <i class="fas fa-user me-2"></i>Profile
+                            </a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button type="submit" class="dropdown-item">
+                                        <i class="fas fa-sign-out-alt me-2"></i>Logout
+                                    </button>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @else
+                    <!-- Guest Menu -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">
+                            <i class="fas fa-sign-in-alt"></i>
+                            <span class="d-none d-lg-inline ms-1">Login</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">
+                            <i class="fas fa-user-plus"></i>
+                            <span class="d-none d-lg-inline ms-1">Register</span>
+                        </a>
+                    </li>
+                @endauth
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
 
     <main class="py-4">
         @yield('content')
